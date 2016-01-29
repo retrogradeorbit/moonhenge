0ef7c9310f4078cfea72ecd040343b94df95f747
Crispin Wellington <retrogradeorbit@gmail.com>
2016-01-29 23:08:47 +0800

    assets definition

---
+++ src/moonhenge/assets.cljs
@@ -0,0 +1,18 @@
+(ns moonhenge.assets)
+
+(def sprites-assets
+  {:star-1
+   {:pos [8 8]
+    :size [8 8]}
+
+   :star-2
+   {:pos [16 8]
+    :size [8 8]}
+
+   :star-3
+   {:pos [24 8]
+    :size [8 8]}
+
+   :star-4
+   {:pos [32 8]
+    :size [8 8]}})
