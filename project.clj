6bc47e32d2ad7e4d71c64fe37ca7b3505444063e
Crispin Wellington <retrogradeorbit@gmail.com>
2016-01-29 21:41:57 +0800

    first cut

--- project.clj
+++ project.clj
@@ -5,12 +5,13 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :min-lein-version "2.5.3"
-  
+
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.170"]
                  [org.clojure/core.async "0.2.374"
-                  :exclusions [org.clojure/tools.reader]]]
-  
+                  :exclusions [org.clojure/tools.reader]]
+                 [infinitelives.pixi/infinitelives.pixi "0.1.0-SNAPSHOT"]]
+
   :plugins [[lein-figwheel "0.5.0-6"]
             [lein-cljsbuild "1.1.2" :exclusions [[org.clojure/clojure]]]]
 
