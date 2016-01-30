4c64929b8a00dbbbd9c07e36a244e45762f9941d
Crispin Wellington <retrogradeorbit@gmail.com>
2016-01-30 13:03:34 +0800

    flying around

--- src/moonhenge/game.cljs
+++ src/moonhenge/game.cljs
@@ -14,13 +14,16 @@
   (:require-macros [cljs.core.async.macros :refer [go]]
                    [infinitelives.pixi.macros :as m]))
 
-(def rotate-speed 0.08)
-
 (def state
   (atom
    {
     :pos (vec2/zero)
     :vel (vec2/zero)
+    :acc (vec2/zero)
+    :max-speed 0.05
+    :thrust 0.003
+    :drag 0.99
+    :rotate-speed 0.08
     }))
 
 (defn left? []
@@ -28,6 +31,9 @@
 
 (defn right? []
   (events/is-pressed? :right))
+
+(defn thrust? []
+  (events/is-pressed? :up))
 
 (defn quit? []
   (events/is-pressed? :q))
@@ -42,16 +48,41 @@
       (starfield/set-position! (-> state deref :pos vec2/as-vector))
 
       ;; update atom pos?
-      (swap! state update-in [:pos] #(vec2/add % (vec2/vec2 0 0.1)) )
+      (swap! state
+             (fn [s]
+               (-> s
+                   ;; update
+                   (update-in [:vel] #(vec2/add % (:acc s)))
+                   (update-in [:pos] #(vec2/add % (:vel s)))
+
+                   ;; drag
+                   (update-in [:vel] #(vec2/scale % (:drag s)))
+
+                   ;; thrust
+                   (update-in [:acc]
+                              #(if (thrust?)
+                                 ;; thrusting
+                                 (-> %
+                                     (vec2/add
+                                      (vec2/rotate
+                                       (vec2/vec2 0 (:thrust s))
+                                       heading))
+                                     (vec2/truncate (:max-speed s))
+                                     )
+
+                                 ;; nothing
+                                 (vec2/zero)
+
+                                 )))))
 
       (when-not (quit?)
         (recur
          (cond
            (left?)
-           (- heading rotate-speed)
+           (- heading (:rotate-speed @state))
 
            (right?)
-           (+ heading rotate-speed)
+           (+ heading (:rotate-speed @state))
 
            :default
            heading))))))
