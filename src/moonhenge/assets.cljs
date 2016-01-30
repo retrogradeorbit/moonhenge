ebeea9faf50d6afd10971f2aeb686c4b85b14c67
Chris McCormick <chris@mccormick.cx>
2016-01-30 13:56:49 +0800

    Enemy ships in assets def.

--- src/moonhenge/assets.cljs
+++ src/moonhenge/assets.cljs
@@ -40,6 +40,30 @@
    {:pos [0 0]
     :size [16 16]}
 
+   :enemy-1
+   {:pos [16 0]
+    :size [16 16]}
+
+   :enemy-2
+   {:pos [32 0]
+    :size [16 16]}
+
+   :enemy-3
+   {:pos [48 0]
+    :size [16 16]}
+
+   :enemy-4
+   {:pos [64 0]
+    :size [16 16]}
+
+   :enemy-5
+   {:pos [80 0]
+    :size [16 16]}
+
+   :enemy-6
+   {:pos [96 0]
+    :size [16 16]}
+
    :title-words
    {:pos [0 32]
     :size [288 32]}})
